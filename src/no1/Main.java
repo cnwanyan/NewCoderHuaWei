206786e45b5dcde2a3e1c1f9eef1f12536a5aa63
wyxccs <wyxccs@163.com>
2017-04-18 09:56:37 +0800

    no1

---
+++ src/no1/Main.java
@@ -0,0 +1,18 @@
+package no1;
+
+import java.util.Scanner;
+/**
+ * 字符串最后一个单词的长度
+ * @author cs
+ *
+ */
+public class Main{
+
+
+    public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String str =sc.nextLine();
+		String[] sp=str.split(" ");
+		System.out.print(sp[sp.length-1].length());
+    }
+}
