2fd91152c0784af53bb6dcd95090d55004587c8b
wyxccs <wyxccs@163.com>
2017-04-18 09:59:12 +0800

    no6

---
+++ src/no6/Main.java
@@ -0,0 +1,31 @@
+package no6;
+
+import java.util.Scanner;
+/**
+ * 质因分子
+ * @author cs
+ *
+ */
+public class Main{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        long number=0;
+        while(sc.hasNextLong()){
+            	number=sc.nextLong();
+            	getReString(number);
+        };
+    }
+    
+    public static String getReString(long ulDataInput){
+    	while(ulDataInput!=1){
+    		for (int i = 2; i <= ulDataInput; i++) {
+				if(ulDataInput%i==0){
+					ulDataInput/=i;
+					System.out.print(i+" ");
+					break;
+				}
+			}
+    	}
+    	return null;
+    }
+}
