2761cf7797662b5381867f8fee2016df9f17a5c7
wyxccs <wyxccs@163.com>
2017-04-18 09:57:40 +0800

    no2

---
+++ src/no2/Main.java
@@ -0,0 +1,24 @@
+package no2;
+
+import java.util.Scanner;
+
+/**
+ * 计算字符个数
+ * @author cs
+ *
+ */
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str =sc.nextLine().toLowerCase();
+        char sd=sc.nextLine().substring(0,1).toLowerCase().charAt(0);
+        char[] hh=str.toCharArray();
+        int ret=0;
+        for (int i = 0; i < hh.length; i++) {
+            if(hh[i]==sd){
+                ++ret;
+            }
+        }
+        System.out.print(ret);
+    }
+}
